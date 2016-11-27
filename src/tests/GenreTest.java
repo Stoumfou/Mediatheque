44b84768dbc938b22881cd755f6bb62e46dbbaf4
Bouveti <bouveti94@gmail.com>
2016-11-27 18:47:30 +0100

    Test Genre

--- src/tests/GenreTest.java
+++ src/tests/GenreTest.java
@@ -2,13 +2,34 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import mediatheque.Genre;
 
 public class GenreTest {
 
+	private String name;
+	private String rename;
+	
+	private Genre genre;
+	
+	@Before
+	public void init(){
+		this.name = "Corde";
+		this.rename = "Tabouret";
+		
+		this.genre = new Genre(this.name);
+		this.genre.emprunter();
+		this.genre.modifier(this.rename);
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		assertEquals(this.rename, this.genre.getNom());
+		assertEquals(2,this.genre.getNbEmprunts());
+		
 	}
 
 }
